a8a09e24540935c103d9609e83bef07f64bcd74c
Hans Hanley <hhanley@DESKTOP-4BB4I68.localdomain>
2021-01-10 14:39:36 -0800

    Final changes

--- buddy-matching.py
+++ buddy-matching.py
@@ -1,15 +1,161 @@
-import pandas 
+import pandas as pd
 import csv 
 import random 
+from enum import Enum
 
-import shared
+
+class Student:
+	def __init__(self, email, name, year, on_campus, time_zone, meeting_freq, meeting_time, classes, major):
+		self.email = email 
+		self.name = name
+		self.year = year
+		self.on_campus = on_campus
+		self.time_zone = time_zone
+		self.meeting_freq = meeting_freq
+		self.meeting_time = meeting_time
+		self.classes = classes
+		self.major = major 
+
+	def __eq__(self, other): 
+		return self.__dict__ == other.__dict__
+
+time_zones = {
+	'UTC-12:00 International Date Line West' : 12,
+	'UTC-11:00 Coordinated Universal Time-11': 13,
+	'UTC-10:00 Aleutian Islands T': 14,
+	'UTC-09:00 Alaska': 15,
+	'UTC-08:00 Pacific Time':16,
+	'UTC-07:00 Mountain Time': 17,
+	'UTC-06:00 Central Time': 18,
+	'UTC-05:00 Eastern Time': 19,
+	'UTC-04:00 Atlantic Time': 20,
+	'UTC-03:00 Brasilia': 21,
+	'UTC-02:00 Coordinated Universal Time-02':22,
+	'UTC-01:00 Azores': 23,
+	'UTC-00:00 London': 0,
+	'UTC+01:00 Berlin, Paris': 1,
+	'UTC+02:00 Jerusalem': 2,
+	'UTC+03:00 Moscow': 3,
+	'UTC+04:00 Abu Dhabi': 4,
+	'UTC+05:00 Islamabad': 5,
+	'UTC+06:00 Astana': 6,
+	'UTC+07:00 Bangkok, Jakarta': 7,
+	'UTC+08:00 Beijing, Hong Kong': 8,
+	'UTC+09:00 Tokyo, Osaka, Seoul': 9,
+	'UTC+10:00 Guam': 10,
+	'UTC+11:00 Chokurdakh': 11,
+	'UTC+12:00 Coordinated Universal Time+12': 12,
+	'UTC+13:00 Coordinated Universal Time+13': 13,
+	'UTC+14:00 Coordinated Universal Time+14': 14,
+}
+
+meeting_frequency = {
+	'Daily': 1,
+	'Weekly': 2,
+	'Monthly': 3,
+}
+
+meeting_times = {
+	'Morning 08:00-12:00': 10, 
+	'Afternoon 12:00-16:00': 14,
+	'Evening 16:00- 20:00': 18,
+	'Night 20:00- 24:00': 22,
+}
+
+
+major_types = {
+	'Undeclared': 0,
+	'Aeronautics and Astronautics' : 1,
+	'African and African American Studies': 3, 
+	'African Studies': 3, 
+	'American Studies': 3, 
+	'Anthropology': 3, 
+	'Applied and Engineering Physics' :1 , 
+	'Archaeology': 3, 
+	'Architectural Design': 3, 
+	'Art History': 4, 
+	'Art Practice': 4, 
+	'Asian American Studies': 3, 
+	'Atmosphere/Energy': 1, 
+	'Bioengineering' : 1,
+	'Biology': 2,
+	'Biomechanical Engineering' : 1,
+	'Biomedical Computation': 2, 
+	'Chemical Engineering' :1 ,
+	'Chemistry': 2, 
+	'Chicana/o - Latina/o Studies': 3, 
+	'China Studies': 3, 
+	'Civil and Environmental Engineering' : 1,
+	'Classics': 4, 
+	'Communication': 4, 
+	'Community Health and Prevention Research': 2, 
+	'Comparative Literature': 4, 
+	'Comparative Studies in Race and Ethnicity': 4, 
+	'Computer Science' : 1,
+	'Democracy, Development, and the Rule of Law': 3, 
+	'Earth Systems': 2, 
+	'Economics': 3, 
+	'Education': 3, 
+	'Electrical Engineering' : 1,
+	'Energy Resources Engineering' :1,
+	'Engineering Physics' : 1, 
+	'English': 4, 
+	'Environmental Systems Engineering' :1,
+	'Ethics in Society': 3, 
+	'Feminist, Gender, and Sexuality Studies'
+	'Film and Media Studies': 4, 
+	'French': 4, 
+	'Geological Studies': 2, 
+	'Geophysics': 2, 
+	'German Studies'
+	'History': 3, 
+	'Honors in the Arts'
+	'Human Biology': 2, 
+	'Iberian and Latin American Cultures': 3, 
+	'International Policy Studies': 3, 
+	'International Relations': 3, 
+	'International Security Studies': 3, 
+	'Italian': 4, 
+	'Japanese': 4, 
+	'Jewish Studies': 3, 
+	'Korean': 4, 
+	'Laboratory Animal Science': 2, 
+	'Latin American Studies': 3, 
+	'Linguistics': 4, 
+	'Management Science and Engineering' : 1,
+	'Materials Science and Engineering': 1,
+	'Mathematical and Computational Science': 1,
+	'Mathematics': 2, 
+	'Mechanical Engineering': 1,
+	'Modern Thought and Literature'
+	'Music': 4, 
+	'Music, Science, and Technology': 2, 
+	'Native American Studies': 3, 
+	'Philosophy': 4, 
+	'Philosophy and Religious Studies'
+	'Physics': 2, 
+	'Political Science': 3, 
+	'Product Design': 1,
+	'Psychology': 2, 
+	'Public Policy': 3, 
+	'Religious Studies': 3, 
+	'Science, Technology, and Society': 2, 
+	'Slavic Languages and Literatures': 4, 
+	'Sociology': 3, 
+	'Spanish': 4, 
+	'Statistics' : 2, 
+	'Symbolic Systems': 1, 
+	'Theater and Performance Studies': 4, 
+	'Urban Studies': 3
+}
+
 
 compatability_scores = dict()
 
-def compute_similarity_score(student1, student2)
+def compute_similarity_score(student1, student2):
 	score = 0
 	num_classes_same = [value for value in student1.classes if value in student2.classes] 
-	score = 0.25 *num_classes_same
+	score = 0.25 *len(num_classes_same)
 	meeting_time1 = (student1.time_zone + student1.meeting_time)%24
 	meeting_time2 = (student2.time_zone + student2.meeting_time)%24
 	if abs(meeting_time1-meeting_time2) < 4:
@@ -17,8 +163,11 @@ def compute_similarity_score(student1, student2)
 	else:
 		score = abs(meeting_time1-meeting_time2) *0.1
 	score = score - (student1.meeting_freq -student2.meeting_freq)*.1
-	if student1.on_campus && student2.on_campus:
+	if student1.on_campus and student2.on_campus:
 		score = score + 0.1 
+	diff_in_maj = abs(major_types[student1.major] - major_types[student2.major])
+	score += (0.4- diff_in_maj*0.1)
+	return score
 
 
 def calculate_all_scores(students):
@@ -50,38 +199,39 @@ def get_current_score(matches,scores):
 	overall_score = 0 
 	for match in matches:
 		student1, student2 = match
-		score = scores[student1][student2]
+		score = scores[student1.email][student2.email]
 		overall_score +=score	
 	return overall_score
 
-def random_assign(students, scores):
+def random_assign(students):
 	all_students = []
 	for student in students:
 		all_students.append(student)
 	matches = []
-	while all_students > 1:
+	while len(all_students) > 1:
 		match1,match2 = random.sample(all_students,2)
 		all_students.remove(match1)
-		all_stuents.remvove(match2)
+		all_students.remove(match2)
 		matches.append([match1,match2])
+	return matches 
 
 def random_change(matches,scores):
 	m1, m2 = random.sample(set(range(len(matches))), 2)
 	match1 = matches[m1]
 	match2 = matches[m2]
 	current_score = get_current_score([match1,match2],scores)
-	changed_match1 = [match1[0] +match2[1]]
-	changed_match2 = [match1[1] +match2[0]]
+	changed_match1 = [match1[0] ,match2[1]]
+	changed_match2 = [match1[1] ,match2[0]]
 	first_new_score = get_current_score([changed_match1,changed_match2],scores)
-	changed_match3 = [match1[0] +match2[0]]
-	changed_match4 = [match1[1] +match2[1]]
+	changed_match3 = [match1[0] ,match2[0]]
+	changed_match4 = [match1[1] ,match2[1]]
 	second_new_score =  get_current_score([changed_match3,changed_match4],scores)
-	if second_new_score > first_new_score && second_new_score > current_score:
+	if second_new_score > first_new_score and second_new_score > current_score:
 		matches.remove(match1)
 		matches.remove(match2)
 		matches.append(changed_match3)
 		matches.append(changed_match4)
-	elif first_new_score > second_new_score && first_new_score > current_score:
+	elif first_new_score > second_new_score and first_new_score > current_score:
 		matches.remove(match1)
 		matches.remove(match2)
 		matches.append(changed_match1)
@@ -91,30 +241,40 @@ def random_change(matches,scores):
 
 def main():
 	## import data 
-	student_data = pd.read_csv('Accountability Buddy Matching Survey  (Responses) - Form Responses 1.csv')
+	student_data = pd.read_csv('Accountability Buddy Matching Survey .csv')
 	students =  list()
+	student_data.rename(columns=lambda x: x.strip(),inplace=True)
 	## Create list of students to compute compatability scores for each of the students
 	for index, student in student_data.iterrows():
-		name = student['Full Name']
-		year = student['Year']
-		on_campus = student['Will you be on-campus during the Spring term?']
-		time_zone = student['Time Zone (Time Zone that you will be located in during the Spring term)']
+		name = student['Full Name'].strip()
+		year = student['Year'].strip()
+		email =  student['Email Address'].strip()
+		on_campus = student['Will you be on-campus during the Spring term?'].strip()
+		time_zone = student['Time Zone (Time Zone that you will be located in during the Spring term)'].strip()
 		time_zone = time_zones[time_zone]
-		meeting_freq = student['How often do you wish to meet?']
+		meeting_freq = student['How often do you wish to meet?'].strip()
 		meeting_freq = meeting_frequency[meeting_freq]
-		meeting_time = student['Preferred Meeting Time']
+		meeting_time = student['Preferred Meeting Time'].strip()
 		meeting_time = meeting_times[meeting_time]
-		if on_campus == 'Yes'
-			on_campus = True:
+		if on_campus == 'Yes':
+			on_campus = True
 		else:
 			on_campus = False 
-		class1 = student['Accountability Courses: Please List courses for which you would want an accountability partner  [Class 1]']
-		class2 = student['Accountability Courses: Please List courses for which you would want an accountability partner  [Class 2]']
-		class2 = student['Accountability Courses: Please List courses for which you would want an accountability partner  [Class 3]']
-		class4 = student['Accountability Courses: Please List courses for which you would want an accountability partner  [Class 4]']
-		major = student['What is your Major?']
+		classes = set()
+		class1 = student['Accountability Courses: Please list courses for which you would want an accountability partner (Course 1)'].strip() 
+		classes.add(class1.strip())
+		class2 = student['Accountability Courses: Please list courses for which you would want an accountability partner (Course 2)']
+		if isinstance(class2, str) and  len(str(class2)) > 4 :
+			classes.add(class2.strip())
+		class3 = student['Accountability Courses: Please list courses for which you would want an accountability partner (Course 3)']
+		if isinstance(class3, str) and  len(str(class3)) > 4 :
+			classes.add(class3)
+		class4 = student['Accountability Courses: Please list courses for which you would want an accountability partner (Course 4)']
+		if isinstance(class4, str) and  len(str(class4)) > 4 :
+			classes.add(class4)
+		major = student['What is your Major?'].strip()
 		classes = list(set({class1,class2,class3,class4}))
-		newStudent = Student(name = name, year =year, 
+		newStudent = Student(name = name, year =year, email = email,
 			on_campus = on_campus,
 			time_zone = time_zone,
 			meeting_freq = meeting_freq,
@@ -123,8 +283,36 @@ def main():
 			major = major)
 
 		students.append(newStudent)
-	best_score = positive_infnity = float('inf') 
-	cacluate_scores = calculate_all_scores(students)
+	best_score = float('inf') 
+	compatability_scores = calculate_all_scores(students)
+	matches = random_assign(students )
+	current_score = get_current_score(matches,compatability_scores)
+	num_rounds = 10 
+	num_students = len(students)
+	for i in range(num_rounds):
+		for j in range(len(students)):
+			matches = random_change(matches, compatability_scores)
+		current_score = get_current_score(matches, compatability_scores)
+		print('Current overall_score: ' + str(current_score))
+
+	filename = "matches.csv"
+	fields = ['Student 1', 'Student 2', 'Student 1 Email', 'Student 2 Email']
+
+	write_matches = []
+	for match in matches:
+		write_matches.append([match[0].name,match[1].name,match[0].email,match[1].email])
+
+	with open(filename, 'w') as csvfile:  
+	    # creating a csv writer object  
+	    csvwriter = csv.writer(csvfile)  
+	        
+	    # writing the fields  
+	    csvwriter.writerow(fields)  
+	        
+	    # writing the data rows  
+	    csvwriter.writerows(write_matches)
+
+
 
 
 
